312d01ec8fe398f58aa86c8bc66b1d767413a2e4
manojkumarksoftsuave <manojkumark.softsuave@gmail.com>
2026-04-01 16:56:51 +0530

    package.json updated

--- playwright.config.js
+++ playwright.config.js
@@ -26,11 +26,13 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- support/hooks.js
+++ support/hooks.js
@@ -1,29 +1,58 @@
 const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 const { chromium } = require('playwright');
+const fs = require('fs');
 
-// Increase the default step timeout for slower environments
 setDefaultTimeout(60000);
 
 Before(async function () {
-  if (typeof this.setDefaultTimeout === 'function') {
-    this.setDefaultTimeout(60000);
-  }
-
-  console.log('Launching browser...');
-
   this.browser = await chromium.launch({
-    headless: false,   
-    slowMo: 500       
+    headless: false,
+    slowMo: 500
   });
 
-  this.context = await this.browser.newContext();
-  this.page = await this.context.newPage();
+  this.context = await this.browser.newContext({
+    recordVideo: {
+      dir: 'reports/videos/'
+    }
+  });
 
-  console.log('Browser launched');
+  // START TRACE
+  await this.context.tracing.start({
+    screenshots: true,
+    snapshots: true
+  });
+
+  this.page = await this.context.newPage();
 });
 
-After(async function () {
-  
-  console.log('Closing browser...');
+After(async function (scenario) {
+  const fileName = scenario.pickle.name.replace(/ /g, "_");
+
+  //  Screenshot on failure
+  if (scenario.result.status === 'FAILED') {
+    const screenshot = await this.page.screenshot({
+      path: `reports/screenshots/${fileName}.png`
+    });
+
+    await this.attach(screenshot, 'image/png');
+  }
+
+  // Handle video
+  const video = this.page.video();
+  if (video) {
+    const videoPath = await video.path();
+
+    if (scenario.result.status === 'FAILED') {
+      const newPath = `reports/videos/${fileName}.webm`;
+      fs.renameSync(videoPath, newPath);
+    } else {
+      fs.unlinkSync(videoPath);
+    }
+  }
+
+  await this.context.tracing.stop({
+    path: `reports/traces/${fileName}.zip`
+  });
+
   await this.browser.close();
 });
